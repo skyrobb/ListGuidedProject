8b8d87101911b64240e0d08c5632289d112af897
Skyler Robbins <ssssssssssssss>
2024-11-07 08:39:08 -0700

    Completed Part 3

--- ListGuidedProject/ToDoTableViewController.swift
+++ ListGuidedProject/ToDoTableViewController.swift
@@ -19,6 +19,8 @@ class ToDoTableViewController: UITableViewController {
         } else {
             toDos = ToDo.loadSampleToDos()
         }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -43,25 +45,21 @@ class ToDoTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            toDos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
