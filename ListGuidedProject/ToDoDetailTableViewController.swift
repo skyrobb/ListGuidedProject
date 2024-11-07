ce8a1bcecb5a72757faa9015721177edb065a34f
Skyler Robbins <ssssssssssssss>
2024-11-07 10:21:32 -0700

    Completed Part 5

---
+++ ListGuidedProject/ToDoDetailTableViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ListGuidedProject
+//
+//  Created by Skyler Robbins on 11/7/24.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var dueDateLabel: UILabel!
+    @IBOutlet weak var dueDateDatePicker: UIDatePicker!
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    var isDatePickerHidden = true
+    let dateLabelIndexPath = IndexPath(row: 0, section: 1)
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesIndexPath = IndexPath(row: 0, section: 2)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dueDateDatePicker.date = Date().addingTimeInterval(24*60*60)
+        updateSaveButtonState()
+        updateDueDateLabel(date: dueDateDatePicker.date)
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func updateDueDateLabel(date: Date) {
+        dueDateLabel.text = date.formatted(.dateTime.month(.defaultDigits).day().year(.twoDigits).hour().minute())
+    }
+    
+    func updateSaveButtonState() {
+        let shouldEnableSaveButton = titleTextField.text?.isEmpty == false
+        saveButton.isEnabled = shouldEnableSaveButton
+    }
+    
+    @IBAction func textEditingChanged(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    @IBAction func returnPressed(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        isCompleteButton.isSelected.toggle()
+    }
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDueDateLabel(date: sender.date)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath where isDatePickerHidden == true:
+            return 0
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return 216
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == dateLabelIndexPath {
+            isDatePickerHidden.toggle()
+            updateDueDateLabel(date: dueDateDatePicker.date)
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+}
